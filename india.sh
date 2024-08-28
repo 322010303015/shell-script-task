5f03759df90b847eeeef669e0060bb256519d371
yaswanth <ytalanki@gitam.in>
2024-08-28 13:55:33 +0530

    shell scripting files

---
+++ india.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "task"
+echo
+mkdir -p india/north/odissa
+mkdir -p india/north/jammu
+echo
+mkdir -p india/south/tamilnadu
+mkdir -p india/south/karnataka
+mkdir -p india/south/telngana
+echo
+touch india/north/odissa/srikakulam.txt
+touch india/north/odissa/bhuvaneshwar.txt
+touch india/north/odissa/puri.txt
+echo
+touch india/north/jammu/kashmir.txt
+touch india/north/jammu/katra.txt
+touch india/north/jammu/gulmargh.txt
+echo
+touch india/south/tamilnadu/chennai.txt
+touch india/south/tamilnadu/madurai.txt
+touch india/south/tamilnadu/kanyakumari.txt
+echo
+touch india/south/karnataka/bangalore.txt
+touch india/south/karnataka/mysore.txt
+touch india/south/karnataka/mangalore.txt
+echo
+touch india/south/telngana/hyderbad.txt
+touch india/south/telngana/warangal.txt
+touch india/south/telngana/khammam.txt
---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#this my 1st shell scripting code
+echo "current user" 
+whoami
+echo
+echo "current working directory"
+pwd
+echo
+echo "list files"
+ls
+echo
+echo "file with permisions"
+ls -l
+touch sample.txt
